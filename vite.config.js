457321402adeb72f443561584c77e4141a08f246
Mostafa Monessy <youremail@example.com>
2025-07-26 07:27:55 +0300

    handle hero Section Page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import path from "path";
+import { log } from 'console';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), flowbiteReact()],
+  
 })
